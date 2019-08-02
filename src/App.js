d86c835180dde79873b354b09ada35b629ec41c4
Fernando Brandao <fernando.brandao@adarga.ai>
2019-08-02 08:26:29 +0100

    useContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+import { UserContext } from './index';
 
 export default function App() {
+  const value = useContext(UserContext);
   return (
-    <div>App</div>
+    <div>
+        Hello, {value.username} {value.lastName}
+    </div>
   );
 }
 
