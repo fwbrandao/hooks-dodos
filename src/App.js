206a2be37e6a2d355e389b01104db00681f58093
Fernando Brandao <fernando.brandao@adarga.ai>
2019-08-05 07:59:15 +0100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React, {useContext} from 'react';
+import React, { useContext, useReducer} from 'react';
 import { UserContext } from './index';
 
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action){
+  switch(action.type) {
+    case "increment": {
+      return {
+        count: state.count + 1
+      }
+    }
+    case "decrement": {
+      return {
+        count: state.count - 1
+      }
+    }
+    default:
+      return initialState
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const value = useContext(UserContext);
+
+
   return (
     <div>
-        Hello, {value.username} {value.lastName}
+        count: {state.count}
+        {/* Hello, {value.username} {value.lastName} */}
+        <button className="border p-1" onClick={() => dispatch({ type: "increment" })}>Increment</button>
+        <button className="border p-1" onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
     </div>
   );
 }
