46407300c1d032e79d0688f1071352827ebfd493
Fernando Brandao <fernando.brandao@adarga.ai>
2019-08-06 08:43:35 +0100

    remove todos

---
+++ src/components/TodoForm.js
@@ -0,0 +1,23 @@
+import React, {useState, useContext} from 'react';
+import TodosContext from '../context';
+
+export default function TodoForm() {
+const [todo, setTodo] = useState("");
+const {dispatch} = useContext(TodosContext);
+
+const handleSubmit = event => {
+  event.preventDefault()
+  dispatch({type: 'ADD_TODO', payload: todo})
+  setTodo("")
+}
+
+  return (
+    <form onSubmit={handleSubmit} className="flex justify-center p-5">
+      <input
+        onChange={event => setTodo(event.target.value)}
+        type="text"
+        className="border-black border-solid border-2"
+      />
+    </form>
+  )
+}
